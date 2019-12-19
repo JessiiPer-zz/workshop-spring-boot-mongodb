b6d369cb98eeb7d7e6a2b570f44c3f63ba407692
JessiiPer <jessica_roch@hotmail.com>
2019-12-19 14:57:26 -0300

    Projeção dos dados do autor com DTO

--- src/main/java/com/udemy/workshopmongo/config/Instantiation.java
+++ src/main/java/com/udemy/workshopmongo/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Component;
 
 import com.udemy.workshopmongo.domain.Post;
 import com.udemy.workshopmongo.domain.User;
+import com.udemy.workshopmongo.dto.AuthorDTO;
 import com.udemy.workshopmongo.repository.PostRepository;
 import com.udemy.workshopmongo.repository.UserRepository;
 @Component
@@ -28,15 +29,17 @@ public class Instantiation implements CommandLineRunner{
 		stf.setTimeZone(TimeZone.getTimeZone("GMT"));
 		
 		repoUser.deleteAll();
+		repoPost.deleteAll();
 		
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
-		Post post1 = new Post(null, stf.parse("21/03/208"),"Partiu viagem", "Vou viajar para São Paulo. Abraços!", maria);
-		Post post2 = new Post(null, stf.parse("23/03/2018"),"Bom dia", "Acordei feliz hoje!", maria);
-		
 		repoUser.saveAll(Arrays.asList(maria, alex,bob));
+		
+		Post post1 = new Post(null, stf.parse("21/03/208"),"Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+		Post post2 = new Post(null, stf.parse("23/03/2018"),"Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+		
 		repoPost.saveAll(Arrays.asList(post1,post2));
 		
 	}
